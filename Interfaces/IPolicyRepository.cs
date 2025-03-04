7c59b4c86ef7f5f030448e13b6358fb79cbc21c9
Akshay Godbole <akshay.godbole@neosoftmail.com>
2025-03-04 18:29:21 +0530

    Hackathon Project

---
+++ Interfaces/IPolicyRepository.cs
@@ -0,0 +1,14 @@
+﻿using InsurancePolicyManagementApp.Models;
+using System.Collections.Generic;
+
+namespace InsurancePolicyManagementApp.Interfaces
+{
+    public interface IPolicyRepository
+    {
+        void AddPolicy(Policy policy);
+        void UpdatePolicy(int policyID, Policy updatedPolicy);
+        Policy GetPolicyByID(int policyID);
+        List<Policy> GetAllPolicies();
+        void DeletePolicy(int policyID);
+    }
+}
